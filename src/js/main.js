c82beb30605719e55fd06a2b8f5e98c8f3122971
Konstantin Terekhin <konstantin.teryohin@rubius.com>
2021-08-30 10:57:58 +0700

    adaptive header

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,5 @@
 import { Fancybox, Carousel } from "@fancyapps/ui";
+import $ from 'jquery';
 
 document.addEventListener('DOMContentLoaded', function(){
     const logoCarousel = new Carousel(document.querySelector(".slider-main"), {
@@ -7,5 +8,30 @@ document.addEventListener('DOMContentLoaded', function(){
         slidesPerPage: 1,
         center: false,
         fill: true
+    });
+
+    $('.categories-links li.with-secondary')
+        .on('mouseenter', function () { toggleSubLinks($(this, true)) })
+        .on('mouseleave', function () { toggleSubLinks($(this), false) });
+
+    function toggleSubLinks(el, isOpen) {
+        $('.sub-links').removeClass('opened');
+        let category = el.data('category');
+        $(`.sub-links[data-id=${category}]`).toggleClass('opened', isOpen);
+    }
+
+    $('.burger-btn').on('click', function () {
+        $('.mobile-menu').addClass('opened');
+    });
+
+    $('.mobile-menu-close').on('click', function () {
+        $('.mobile-menu').removeClass('opened');
+    });
+    $('.mobile-menu__backdrop').on('click', () => $('.mobile-menu').removeClass('opened'));
+
+    $('.mobile-menu .arrow-btn').on('click', function () {
+        $(this).toggleClass('active');
+        $(this).closest('li').toggleClass('active');
+        $(this).closest('li').find('.sub-links').slideToggle('opened');
     });
 });
